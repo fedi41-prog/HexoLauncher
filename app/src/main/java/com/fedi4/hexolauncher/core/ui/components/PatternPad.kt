47698e99b40b79ab8a823b17b23c2b7af1467afa
fedi41-prog <crocofedi4@gmail.com>
2026-06-06 11:04:32 +0200

    adding a trail canvas

--- app/src/main/java/com/fedi4/hexolauncher/core/ui/components/PatternPad.kt
+++ app/src/main/java/com/fedi4/hexolauncher/core/ui/components/PatternPad.kt
@@ -3,38 +3,37 @@ package com.fedi4.hexolauncher.core.ui.components
 import android.util.Log
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.graphics.shapes.CornerRounding
-import androidx.graphics.shapes.Morph
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.circle
 import com.fedi4.hexolauncher.core.data.PadPoint
 import com.fedi4.hexolauncher.core.data.PadPointType
 import com.fedi4.hexolauncher.core.ui.HexoPadViewModel
@@ -49,6 +48,8 @@ fun PatternPad(
 ) {
 
     val controller = remember { HexoLayoutController() }
+
+
 
 
     // MORPH STUFF
@@ -76,8 +77,9 @@ fun PatternPad(
     // ===================
 
 
+
     HexoLayout(
-        modifier = modifier.fillMaxWidth().fillMaxHeight()
+        modifier = modifier
             .pointerInput(Unit) {
                 awaitPointerEventScope {
                     while (true) {
@@ -95,48 +97,60 @@ fun PatternPad(
                     }
                 }
             }
-            .pointerInput(Unit) {
-
-                detectDragGestures(
-                    onDragStart = { offset ->
-                        //vm.onDragStart()
-                    },
-                    onDragEnd = {
-                        vm.onDragEnd()
-                    },
-                    onDragCancel = {
-                        vm.onDragCancel()
-                    },
-                    onDrag = { change, dragAmount ->
-
-                    }
-                )
-            }.border(
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragStart = { offset -> //vm.onDragStart() }, onDragEnd = {
+                            vm.onDragEnd() },
+                        onDragCancel = {
+                            vm.onDragCancel() },
+                        onDrag = { change, dragAmount ->
+                        }
+                    )
+                }.border(
                 3.dp,
                 MaterialTheme.colorScheme.surface.copy(1 - animatedProgress.value),
                 CircleShape
-            ),
+                ),
         itemSize = 100.dp,
-        ringRadius = 110.dp,
+        ringRadius = 120.dp,
         onNearestChild = { id ->
-            vm.onTouchedPoint(id)
-        },
+            vm.onTouchedPoint(id) },
         onExitCircle = {
-            vm.onDragCancel()
-        },
-        controller
+            vm.onDragCancel() },
+        controller = controller
     ) {
-
         repeat(7) { i ->
-
             PatternPadPoint(
-                size = 80.dp,
+                size = 85.dp,
                 id = i,
                 vm = vm
             )
-
         }
+    }
+}
 
+
+@Composable
+fun PatternPadWrapper(modifier: Modifier, vm: HexoPadViewModel) {
+
+    var padSize by remember { mutableStateOf(IntSize.Zero) }
+
+    Box(modifier = modifier.size(
+        width = with(LocalDensity.current) { padSize.width.toDp() },
+        height = with(LocalDensity.current) { padSize.height.toDp() }
+    )) {
+
+
+        PatternTrailCanvas(
+            Modifier.size(
+                width = with(LocalDensity.current) { padSize.width.toDp() },
+                height = with(LocalDensity.current) { padSize.height.toDp() }
+            )
+        )
+
+        PatternPad(
+            modifier = Modifier.fillMaxSize().onSizeChanged {padSize = it}, vm = vm
+        )
     }
 }
 
@@ -167,40 +181,64 @@ fun PatternPadPoint(
     //val morph = remember {
     //    Morph(shapeA, shapeB)
     //}
-    val animatedProgress = animateFloatAsState(
-        targetValue = if (isLast) 2f else 0f,
+    val animationTouchingPoint = animateFloatAsState(
+        targetValue = if (isLast) 3f else 0f,
         label = "progress",
         animationSpec = spring(2f, Spring.StiffnessMedium)
     )
+    val animationDragging = animateFloatAsState(
+        targetValue = if (!vm.isDragging.value) 1f else 0.5f,
+        label = "progress",
+        animationSpec = spring(2f, Spring.StiffnessHigh)
+    )
+
     // ===================
 
-
+    val bgColor = Color.Black.copy(1-animationDragging.value)
 
     Box(Modifier.size(size)) {
         if (pointData.type == PadPointType.APP) {
             Image(
                 painter = BitmapPainter(vm.getIcon(pointData.icon!!)),
                 contentDescription = null,
-                modifier = Modifier.size(size).align(Alignment.Center)
+                modifier = Modifier
+                    .size(size)
+                    .align(Alignment.Center)
                     .clip(CircleShape)
-                    .border(3.dp + animatedProgress.value.dp, Color.White, CircleShape)
-                    .background(Color.Black)
+                    .border(3.dp + animationTouchingPoint.value.dp, Color.White, CircleShape)
+                    .background(bgColor, shape = CircleShape)
             )
         } else if (pointData.type == PadPointType.FOLDER) {
             Box(
-                Modifier.size(size).align(Alignment.Center)
-                    .border(3.dp + animatedProgress.value.dp, Color.White, CircleShape)//.padding(size/5)
-                    .background(Color.Black, shape = CircleShape)
+                Modifier
+                    .size(size)
+                    .align(Alignment.Center)
+                    .border(3.dp + animationTouchingPoint.value.dp, Color.White, CircleShape)//.padding(size/5)
+                    .background(bgColor, shape = CircleShape)
             ) {
-                Text(pointData.text, fontSize = 10.sp, color = Color.White, modifier = Modifier.align(Alignment.Center))
+                Text(pointData.text, fontSize = 10.sp, color = Color.White.copy(1.65f-animationDragging.value), modifier = Modifier.align(Alignment.Center))
             }
         } else if (pointData.type == PadPointType.ACTION) { }
         else if (pointData.type == PadPointType.EMPTY) {
             Box(
-                Modifier.size(size).align(Alignment.Center)
-                    .border(1.dp + animatedProgress.value.dp, Color.White, CircleShape)
-                    .background(Color.Black, shape = CircleShape)
+                Modifier
+                    .size(size)
+                    .align(Alignment.Center)
+                    .border(1.dp + animationTouchingPoint.value.dp, Color.White, CircleShape)
+                    .background(bgColor, shape = CircleShape)
             )
         }
     }
+}
+
+
+
+
+@Preview()
+@Composable
+fun PatternPadPreview() {
+    PatternPadWrapper(
+        modifier = Modifier,
+        vm = HexoPadViewModel(context = LocalContext.current)
+    )
 }
