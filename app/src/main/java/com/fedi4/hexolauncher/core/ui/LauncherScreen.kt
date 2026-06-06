04eec7d210e10cb4d3c9f9369af7afc80556cbda
fedi41-prog <crocofedi4@gmail.com>
2026-06-06 11:04:32 +0200

    adding a trail canvas

--- app/src/main/java/com/fedi4/hexolauncher/core/ui/LauncherScreen.kt
+++ app/src/main/java/com/fedi4/hexolauncher/core/ui/LauncherScreen.kt
@@ -3,33 +3,41 @@ package com.fedi4.hexolauncher.core.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.fedi4.hexolauncher.core.ui.components.PatternPad
+import com.fedi4.hexolauncher.core.ui.components.PatternPadWrapper
 import com.fedi4.hexolauncher.core.ui.components.TimeWidget
 
 @Composable
 fun LauncherScreen(modifier: Modifier, vm: HexoPadViewModel) {
-    val context = LocalContext.current
-
-
 
     Column(
         modifier = modifier.fillMaxSize()
             .padding(bottom = Dp(50f))
             .padding(16.dp)
     ) {
-        TimeWidget(modifier = Modifier.weight(1f))
-        Spacer(modifier = Modifier.weight(1f))
-        PatternPad(
-            modifier = Modifier.weight(1f),
+        TimeWidget(modifier = Modifier.weight(0.7f))
+        Spacer(modifier = Modifier.weight(0.7f))
+        PatternPadWrapper(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             vm = vm
         )
     }
 
+}
+
+
+@Preview()
+@Composable
+fun LauncherScreenPreview() {
+    LauncherScreen(modifier = Modifier.fillMaxSize(), vm = HexoPadViewModel(context = LocalContext.current))
 }
 
---
+++ app/src/main/java/com/fedi4/hexolauncher/core/ui/components/PatternTrailCanvas.kt
@@ -0,0 +1,23 @@
+package com.fedi4.hexolauncher.core.ui.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PatternTrailCanvas(modifier: Modifier) {
+
+
+    Canvas(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        drawRect(
+            color = Color.Blue,
+        )
+    }
+}
